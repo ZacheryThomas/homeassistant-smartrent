550f66d61ac884fa4170e07c8ff3933481496663
ZacheryThomas <zacherythomas12@gmail.com>
2022-06-07 09:59:15 -0400

    sensor: Add support for sensors

--- custom_components/smartrent/const.py
+++ custom_components/smartrent/const.py
@@ -4,5 +4,5 @@ CONFIGURATION_URL = "https://control.smartrent.com/login"
 
 CONF_PASSWORD = "password"
 CONF_USERNAME = "username"
-PLATFORMS = ["lock", "climate", "switch", "binary_sensor"]
+PLATFORMS = ["lock", "climate", "switch", "binary_sensor", "sensor"]
 STARTUP_MESSAGE = f"Starting setup for {DOMAIN}"
---
+++ custom_components/smartrent/sensor.py
@@ -0,0 +1,120 @@
+"""Platform for sensor integration."""
+import logging
+from typing import Union, Optional
+
+from smartrent import DoorLock, LeakSensor, Thermostat
+from smartrent.api import API
+
+_LOGGER = logging.getLogger(__name__)
+
+from homeassistant.helpers.device_registry import DeviceEntryType
+from homeassistant.components.sensor import SensorEntity, SensorStateClass
+from homeassistant.const import (PERCENTAGE, TEMP_FAHRENHEIT)
+
+
+from .const import PROPER_NAME, CONFIGURATION_URL
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Setup sensor platform."""
+    devs: Optional[Union[DoorLock, Thermostat]] = []
+    client: API = hass.data["smartrent"][entry.entry_id]
+
+    devs = client.get_device_list()
+
+    sensors_dict = {
+        Thermostat: {
+            'temperature': [
+                'current_temp',
+            ],
+            'humidity': [
+                'current_humidity',
+            ],
+            'misc': [
+                'fan_mode',
+                'mode',
+            ]
+        },
+        DoorLock: {
+            'battery': [
+                'battery_level',
+            ],
+            'misc': [
+                'notification',
+                'locked',
+            ]
+        },
+        LeakSensor: {
+            'battery': [
+                'battery_level',
+            ]
+        }
+    }
+
+    for dev in devs:
+        device = sensors_dict.get(type(dev))
+        for device_class in device.keys():
+            for sensor_name in device[device_class]:
+                device_class = None if device_class == 'misc' else device_class
+                async_add_entities([SensorEnt(dev, sensor_name, device_class)])
+
+class SensorEnt(SensorEntity):
+    def __init__(self, device: Union[DoorLock, Thermostat, LeakSensor], sensor_name: str, device_class: str) -> None:
+        super().__init__()
+        self.device = device
+        self.sensor_name = sensor_name
+        self._device_class = device_class
+
+        self.device.start_updater()
+        self.device.set_update_callback(self.async_schedule_update_ha_state)
+
+    @property
+    def available(self) -> bool:
+        return self.device.get_online()
+
+    @property
+    def should_poll(self):
+        """Return the polling state, if needed."""
+        return False
+
+    @property
+    def unique_id(self):
+        sname_id = ''.join([str(ord(char)) for char in self.sensor_name])
+        uid = str(self.device._device_id) + str(sname_id)
+
+        return uid
+
+    @property
+    def name(self):
+        """Return the display name of this sensor."""
+        return self.device._name + ' ' + self.sensor_name
+
+    @property
+    def native_value(self):
+        """Return native value for entity."""
+        return getattr(self.device, f'get_{self.sensor_name}')()
+
+    @property
+    def device_class(self):
+        return self._device_class
+
+    @property
+    def state_class(self):
+        return SensorStateClass.MEASUREMENT if self._device_class else None
+
+    @property
+    def native_unit_of_measurement(self):
+        if self._device_class == 'temperature':
+            return TEMP_FAHRENHEIT
+        if self._device_class in ['humidity', 'battery']:
+            return PERCENTAGE
+
+    @property
+    def device_info(self):
+        return dict(
+            identifiers={("id", self.device._device_id)},
+            manufacturer=PROPER_NAME,
+            model=str(self.device.__class__.__name__),
+            entry_type=DeviceEntryType.SERVICE,
+            configuration_url=CONFIGURATION_URL,
+        )
+
