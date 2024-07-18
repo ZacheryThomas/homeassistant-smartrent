06986ba400447a9456ce9d16a8d2fb3b6343c680
ZacheryThomas <zacherythomas12@gmail.com>
2024-07-18 16:56:13 -0400

    init: Fix warnings about dep functions
    https://github.com/ZacheryThomas/homeassistant-smartrent/issues/28
    
    https://github.com/ZacheryThomas/homeassistant-smartrent/issues/27

--- custom_components/smartrent/__init__.py
+++ custom_components/smartrent/__init__.py
@@ -50,10 +50,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = api
 
-    for platform in PLATFORMS:
-        hass.async_add_job(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.add_update_listener(async_reload_entry)
     return True
